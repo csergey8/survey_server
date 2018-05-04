130a86b75a5c454c0c34149732ed49a310fc62c6
csergey8 <c.sergey@protonmail.com>
2018-05-04 14:36:52 +0300

    added serialize and deSerialize passport functions

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,21 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, userd.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        })
+});
+
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
@@ -8,5 +23,18 @@ passport.use(new GoogleStrategy({
     callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-        console.log(accessToken);
+        User.findOne({ googleId: profile.id })
+            .then((existingUser) => {
+                if (existingUser) {
+
+                    done(null, existingUser);
+                } else {
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user));
+                }
+            })
+
+
+        
 }));
