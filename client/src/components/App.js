bb5081a40426540d997730436016c7a73b140655
csergey8 <c.sergey@protonmail.com>
2018-05-07 14:52:58 +0300

    reducer added and refactor acrion

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 
@@ -8,19 +10,26 @@ const Dashboard = () => <h1>Dashboard</h1>
 const SurveyNew = () => <h1>SurveyNew</h1>
 const Landing = () => <h1>Landing</h1>
 
-const App = () => {
-    return (
-        <div>
-           <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route exact path="/" component={Landing} />
-                    <Route exact path="/surveys" component={Dashboard} />
-                    <Route path="/surveys/new" component={SurveyNew} />
-                </div>
-           </BrowserRouter>
-        </div>
-    );
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+    render() {
+        return (
+            <div className="container">
+               <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/surveys" component={Dashboard} />
+                        <Route path="/surveys/new" component={SurveyNew} />
+                    </div>
+               </BrowserRouter>
+            </div>
+        );
+    }
 }
+    
 
-export default App;
+
+export default connect(null, actions)(App);
